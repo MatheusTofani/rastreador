566e8c3906353980211a4b99a25163e25edcf0a5
Matheus <matheus271112@gmail.com>
2024-09-15 22:46:00 -0300

    finalizando testes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,25 @@
-document.getElementById('buscarCep').addEventListener('click', function () {
-    const cep = document.getElementById('cep').value.trim();
-    const resultado = document.getElementById('resultado');
+document.querySelector('.box__form__cep__button').addEventListener('click', function (e) {
+    e.preventDefault();  // Previne a atualização da página
+    
+    const cep = document.querySelector('.box__form__cep__input').value;
+    const inputCidade = document.querySelector('.box__form__cidade__input');
+    const inputEstado = document.querySelector('.box__form__estado__input');
+
+    if (cep === "") {
+        alert('Por favor, insira um CEP.');
+        return;
+    }
 
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
         .then(function (res) {
-
             return res.json();
         })
-
         .then(function (json) {
-
+            console.log(json);
+            inputCidade.value = json.localidade;
+            inputEstado.value = json.uf;
         })
-        .catch(function (error) {
-            console.error('Erro:', error);
-            document.getElementById('resultado').textContent = 'Erro ao buscar o CEP. Tente novamente mais tarde.';
+        .catch(function (err) {
+            console.error('Erro ao buscar CEP:', err);
         });
-
 });
